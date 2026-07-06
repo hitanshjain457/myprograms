e533db923f177e67eb23e7e5b02d43b4caf0b797
hitanshjain457 <hitanshjain757@gmail.com>
2026-07-06 18:50:40 +0530

    new program

--- loopss/loop06.py
+++ loopss/loop06.py
@@ -1,1 +1,13 @@
-#write a program to check whether a number is armstrong number or not.
+#write a program to check whether a number is prime or not
+num=13
+prime=True
+if num>=1:
+    for i in range(2,num):
+        if num%i==0:
+            prime=False
+    if prime:
+        print("is a prime number")
+    else:
+        print("is not a prime number")
+else:
+    print("number is negative or zero")
