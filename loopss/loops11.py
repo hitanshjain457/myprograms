8e692959fdb19bab8928217d368b657f7fac241c
hitanshjain457 <hitanshjain757@gmail.com>
2026-07-09 19:17:27 +0530

    update

--- loopss/loops11.py
+++ loopss/loops11.py
@@ -1,2 +1,8 @@
 #Write a program to enter a number and print its reverse.		
-a="123"
+a=788
+reverse=0
+while(a>0):
+    digit=a%10
+    reverse=reverse*10+digit
+    a=a//10
+print("Reverse of the number:", reverse)
