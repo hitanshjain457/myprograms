dff4232d29c1c1242579113a6af3408f6b19dd52
hitanshjain457 <hitanshjain757@gmail.com>
2026-07-10 19:12:02 +0530

    update

--- loopss/loops16.py
+++ loopss/loops16.py
@@ -1,3 +1,12 @@
-#Write a program to print all Strong numbers between 1 to n.		
+#Write a program to print all armstrong numbers between 1 to n.		
 n=10
 for num in range(1, n + 1):
+    sum=0
+    temp=num
+    while(temp>0):
+        digit=temp%10
+        sum=sum+digit**3
+        temp=temp//10
+    if(num==sum):
+        print(num)
+
