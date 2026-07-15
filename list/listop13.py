fd361c0ece8a31fe9d53972e922dac70a957bdd3
hitanshjain457 <hitanshjain757@gmail.com>
2026-07-15 19:23:59 +0530

    update

--- list/listop13.py
+++ list/listop13.py
@@ -1,12 +1,15 @@
-a = [1, 2, 3, 1, 2, 4, 5, 6, 5]
-dup = []
+a=[1,2,3,4,5,6,5,1,3]
 
 for i in range(len(a)):
-    count=0
-    for j in range(i + 1, len(a)):
-
-        if a[i] == a[j] and a[i] not in dup:
-			
-            dup.append(a[i])
-            count+=1 
-print({a[i]},":", {count})
+    count=1
+    already_counted = False
+    for j in range(i):
+        if (a[i]==a[j]):
+            already_counted = True
+            break
+    if already_counted:
+            continue
+    for j in range(i+1,len(a)):
+        if (a[i] == a[j]):
+            count+=1
+    print(a[i],":",count)
