6722319318593adbe8bf4bf81bdd624d316044d6
hitanshjain457 <hitanshjain757@gmail.com>
2026-09-09 19:35:33 +0530

    new

---
+++ test/file.py
@@ -0,0 +1,31 @@
+# with open("./data.txt","w") as file:
+#     file.write("ok")
+#     file.write("hello")
+#     file.write("hai")
+
+# with open("./data.txt","r") as file:
+#     # data = file.read()
+#     # data = file.read(20)
+
+# print(data)
+
+
+# with open("./data.txt","a") as file:
+#     file.write("siddharth")
+
+# file = open("./data.txt","r")
+# data = file.read()
+# print(data)
+
+# with open("./data.txt","r+") as file:
+#     file.write("ok\n")
+#     file.write("hello\n")
+#     file.write("hai\n")
+#     file.seek(0)
+#     data = file.read()
+#     print(data)
+
+
+
+with open("./data2.txt", "w") as file:
+    file.write("hellosfsf")
